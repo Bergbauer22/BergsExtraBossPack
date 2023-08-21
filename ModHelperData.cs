8829f0da4832276f159fe483a929c80f805e6e95
Bergbauer22 <dieterzeller24@gmail.com>
2023-08-21 18:05:59 +0200

    No negative Cash anymore

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace BergsExtraBossPack;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "38.1";
-    public const string Version = "0.2.2";
+    public const string Version = "0.2.3";
     public const string Name = "BergsExtraBossPack";
 
     public const string Description = "Adds new bosses to the Game[Requires BossIntegration]";
